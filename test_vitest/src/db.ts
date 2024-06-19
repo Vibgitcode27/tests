8e3428d465bdcd4c5ea7495f0860785425c8214d
vibhor <vibgitcode27@gmail.com>
2024-06-19 20:23:08 +0530

    mocks of database calls

--- test_vitest/src/db.ts
+++ test_vitest/src/db.ts
@@ -1,4 +1,9 @@
 // Mocking is easy if the code that is being mocked is in seperate file
 
 import { PrismaClient } from "@prisma/client";
-export const prismaClient = new PrismaClient();
+import { mockDeep } from "vitest-mock-extended";
+// for specific mocking
+// export const prismaClient = new PrismaClient();
+
+// deep mocking
+export const prismaClient = mockDeep<PrismaClient>();
